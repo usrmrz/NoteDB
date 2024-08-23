33cf1878f6e4181606ebea62d048590382b0f5bc
usrmrz <usrmrz@gmail.com>
2024-08-23 12:01:18 +0300

    adding a Spacer 5.db

--- app/src/main/java/dev/usrmrz/notesdb/ui/theme/MainScreen.kt
+++ app/src/main/java/dev/usrmrz/notesdb/ui/theme/MainScreen.kt
@@ -4,8 +4,10 @@ import android.graphics.drawable.Icon
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -54,5 +57,6 @@ fun MainScreen() {
                 )
             }
         }
+        Spacer(modifier = Modifier.height(5.dp))
     }
 }

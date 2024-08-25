80f1f3c0a70a3bda88d3523ec54b1fec5e4d5ce7
usrmrz <usrmrz@gmail.com>
2024-08-25 13:05:43 +0300

    some editing MainViewModel.kt file(Alt+Enter on selection APPLICATION_KEY)

--- app/src/main/java/dev/usrmrz/notesdb/MainViewModel.kt
+++ app/src/main/java/dev/usrmrz/notesdb/MainViewModel.kt
@@ -2,6 +2,7 @@ package dev.usrmrz.notesdb
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewmodel.CreationExtras
 import dev.usrmrz.notesdb.data.MainDb
 
